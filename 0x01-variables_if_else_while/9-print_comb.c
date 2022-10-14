d362fcee533e237733b2e9534a3236dc9c07b67e
sandymaramma <edesandraoge@gmail.com>
2022-10-14 15:38:14 -0700

    Answer 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 /**
  * main - entry point
- *
- * Return: alway returns 0
- *
+ * Return: always returns 0
  */
 int main(void)
 {
@@ -12,10 +10,10 @@ for (i = 10; i < 20; i++)
 {
 putchar((i % 10) + '0');
 if (i != 19)
-}
+{
 putchar(',');
 putchar(' ')
-{
+}
 putchar('\n');
 }
 return (0);
