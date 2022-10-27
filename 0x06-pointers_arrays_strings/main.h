9d837297c67f57039a22e9048cfdd4ea8750c21d
sandymaramma <edesandraoge@gmail.com>
2022-10-27 08:59:35 -0700

    Answer 11

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,10 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+void print_buffer(char *b, int size);
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
 void print_number(int n);
 
 char *rot13(char *s);
