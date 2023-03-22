b24f45ba5389b27139f09109215636d98e01c95c
sandymaramma <edesandraoge@gmail.com>
2023-03-22 14:45:20 -0700

    commit message

---
+++ sandy/operator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main (void)
+{
+
+int num1 = 25, num2 = 5, result;
+
+result = 25/5;
+
+printf("num1 / num2 = %d\n", result);
+
+result = num1%num2;
+
+printf("Remainder when num1 is divided by num2 is = %d\n", result);
+
+return (0);
+
+}
